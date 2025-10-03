81f48d6dad3aba826b37b71e55efc9452b39fc70
Alex Vincent <alex@far2cool.com>
2025-10-04 01:28:21 +0200

    now compiles!

--- src/CasCap.Api.Azure.CognitiveServices/Models/_CognitiveServicesOptions.cs
+++ src/CasCap.Api.Azure.CognitiveServices/Models/_CognitiveServicesOptions.cs
@@ -4,7 +4,7 @@ namespace CasCap.Models;
 
 public record CognitiveServicesOptions
 {
-    public const string SectionKey = $"{nameof(CasCap)}:{nameof(CognitiveServicesOptions)}";
+    public const string ConfigurationSectionName = $"{nameof(CasCap)}:{nameof(CognitiveServicesOptions)}";
 
     [Required]
     public required string SubscriptionKey { get; init; }
