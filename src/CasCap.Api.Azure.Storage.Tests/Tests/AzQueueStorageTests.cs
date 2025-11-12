217bfe0062030d81eff46ba0dc5af84392958e0d
Alex Vincent <alex@far2cool.com>
2025-11-12 03:46:21 +0100

    debug failing test

--- src/CasCap.Api.Azure.Storage.Tests/Tests/AzQueueStorageTests.cs
+++ src/CasCap.Api.Azure.Storage.Tests/Tests/AzQueueStorageTests.cs
@@ -9,22 +9,27 @@ public class AzQueueStorageTests(/*ITestOutputHelper output*/) : TestBase/*(outp
 
         var testObj = new TestMessage { TestString = inputTestString };
 
+        //queue test message #1
         var result1 = await _queueSvc.Enqueue(testObj);
         Assert.True(result1);
 
+        //queue test message #2
         var result2 = await _queueSvc.Enqueue(testObj);
         Assert.True(result2);
 
+        //dequeue test message #1
         var result3 = await _queueSvc.DequeueSingle<TestMessage>();
         Assert.NotNull(result3.obj);
         Assert.Equal(result3.obj.TestString, inputTestString);
 
+        //queue test message #3
         var result4 = await _queueSvc.Enqueue(testObj);
         Assert.True(result4);
 
+        //dequeue test messages #2 & #3
         var result5 = await _queueSvc.DequeueMany<TestMessage>();
         Assert.NotNull(result5);
-        Assert.True(result5.Count > 1);
+        Assert.True(result5.Count >= 2, $"actually found {result5.Count} message(s)");
     }
 }
 
