a5891692db1b172e0a5dfbcb6ea7045c6dc74e9f
Alex Vincent <alex@far2cool.com>
2026-04-15 13:31:42 +0200

    add config validation

--- src/CasCap.Api.Azure.Auth/Models/_AzureAuthConfig.cs
+++ src/CasCap.Api.Azure.Auth/Models/_AzureAuthConfig.cs
@@ -11,7 +11,7 @@ public record AzureAuthConfig : IAppConfig, IAzureAuthConfig
     public static string ConfigurationSectionName => "AppConfig";
 
     /// <inheritdoc/>
-    [Required]
+    [Required, MinLength(3)]
     public required string KeyVaultName { get; init; }
 
     /// <inheritdoc/>
